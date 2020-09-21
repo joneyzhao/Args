3528716530f392812edad13a675ef28093fa0369
zhaohuanhuan <1297972106@qq.com>
2020-09-21 17:30:26 +0800

    增加枚举类ValueType实现及测试

--- src/main/java/ArgsPractice/ValueType.java
+++ src/main/java/ArgsPractice/ValueType.java
@@ -1,4 +1,22 @@
 package ArgsPractice;
 
-public class ValueType {
+public enum ValueType {
+    BOOLEAN("false"),
+    INTEGER("0"),
+    STRING("");
+
+
+    private final Object defaultValue;
+
+    ValueType(Object defaultValue) {
+        this.defaultValue = defaultValue;
+    }
+
+    public Object getDefaultValue() {
+        return this.defaultValue;
+    }
+
+    public String getType() {
+        return this.name();
+    }
 }
---
+++ src/test/java/ArgsPractice/ValueTypeTest.java
@@ -0,0 +1,20 @@
+package ArgsPractice;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ValueTypeTest {
+    @Test
+    public void should_return_DefaultValue_when_getDefaultValue_given_type(){
+
+        assertEquals("BOOLEAN", ValueType.BOOLEAN.getType());
+        assertEquals("false", ValueType.BOOLEAN.getDefaultValue());
+
+        assertEquals("INTEGER", ValueType.INTEGER.getType());
+        assertEquals("0", ValueType.INTEGER.getDefaultValue());
+
+        assertEquals("STRING", ValueType.STRING.getType());
+        assertEquals("", ValueType.STRING.getDefaultValue());
+    }
+}
