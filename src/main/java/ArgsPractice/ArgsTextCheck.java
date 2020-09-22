58ddbb6290d4c0934b13971d5719a116b712f49a
zhaohuanhuan <1297972106@qq.com>
2020-09-22 19:47:09 +0800

    增加对输入字符串的校验:防重复和与flag之间有空格

--- src/main/java/ArgsPractice/ArgsTextCheck.java
+++ src/main/java/ArgsPractice/ArgsTextCheck.java
@@ -1,4 +1,36 @@
 package ArgsPractice;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class ArgsTextCheck {
+    private String argsText;
+    private List<String> schemaFlagList = Arrays.asList("-l", "-p", "-d");
+
+    public ArgsTextCheck(String argsText){
+        this.argsText = argsText;
+    }
+
+    public Boolean antiDuplicationCheck(){
+        for(String schemaFlag: schemaFlagList){
+            int counts = (argsText.length()-argsText.replaceAll(schemaFlag, "").length())/2;
+            System.out.println("1===" + argsText);
+            if(counts > 1){
+                System.out.println("不允许输入重复flag");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public Boolean illegalBlankSpaceBetweenFlagCheck(){
+        Boolean hasIllegalBlankSpaceBetweenFlag = argsText.indexOf("- l") != -1 || argsText.indexOf("- p") != -1 || argsText.indexOf("- d") != -1;
+
+        if(hasIllegalBlankSpaceBetweenFlag){
+            System.out.println("不允许在-和flag存在空格");
+            return false;
+        }
+        return true;
+    }
+
 }
--- src/test/java/ArgsPractice/ArgsTextCheckTest.java
+++ src/test/java/ArgsPractice/ArgsTextCheckTest.java
@@ -1,4 +1,31 @@
 package ArgsPractice;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ArgsTextCheckTest {
+    @Test
+    public void should_return_true_when_antiDuplicationCheck_given_argsText(){
+        ArgsTextCheck argsTextCheck = new ArgsTextCheck("-l true -l 8080 -d user/logs");
+
+        Boolean isDuplication = argsTextCheck.antiDuplicationCheck();
+
+        assertEquals(false, isDuplication);
+
+    }
+
+    @Test
+    public void should_return_true_when_illegalBlankSpaceCheck_given_argsText(){
+        ArgsTextCheck argsTextCheck = new ArgsTextCheck("-l true -l 8080 - d user/logs");
+
+        Boolean isDuplication = argsTextCheck.antiDuplicationCheck();
+
+        Boolean hasIllegalBlankSpace = argsTextCheck.illegalBlankSpaceBetweenFlagCheck();
+
+        assertEquals(false, isDuplication);
+        assertEquals(false, hasIllegalBlankSpace);
+    }
+
+
 }
