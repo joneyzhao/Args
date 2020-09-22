88f4d44a56e6056e3169cf531793ca1db8c59eb8
zhaohuanhuan <1297972106@qq.com>
2020-09-22 16:04:51 +0800

    Args的功能实现

--- src/main/java/ArgsPractice/Args.java
+++ src/main/java/ArgsPractice/Args.java
@@ -6,34 +6,47 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Args {
-    private String argsText;
 
-    public Args(String argsText) {
+    private List<Arg> argList = new ArrayList<>();
+    private String argsText;
+    private Schema schema;
+
+    public Args(String argsText, Schema schema) {
         this.argsText = argsText;
+        this.schema = schema;
     }
 
-    public List<KeyValuePair> scan() {
-        List<String> keyValues = Arrays.asList(argsText.split("-"));
-        keyValues = keyValues.stream()
+    public List mapToArgPairs() {
+        List<String> argPairsList = Arrays.asList(argsText.split("-"));
+        argPairsList = argPairsList.stream()
                 .map(keyValue -> keyValue.trim())
                 .collect(Collectors.toList());
-        keyValues = keyValues.subList(1, keyValues.size());
+        argPairsList = argPairsList.subList(1, argPairsList.size());
 
-        return getKeyValuePairs(keyValues);
+        return argPairsList;
     }
 
-    private List<KeyValuePair> getKeyValuePairs(List<String> keyValues) {
-        List<KeyValuePair> keyValuePairs = new ArrayList<>();
-        keyValues.forEach(keyValue -> {
-            String [] splitKeyValue = keyValue.split(" ");
-            String key = splitKeyValue[0];
-            String value = splitKeyValue[1];
-            keyValuePairs.add(new KeyValuePair(key, value));
+    public List<Arg> getArgList(List<String> argPairs) {
+        argPairs.forEach(argPair -> {
+            argList.add(new Arg(argPair, schema));
         });
-        return keyValuePairs;
+        System.out.println(argList);
+        return argList;
     }
-//
-//    public Object getValueOf(String flag){
-//
-//    }
+
+    public Object getValueOf(String flag) throws Exception {
+        getArgList(mapToArgPairs());
+        return argList.stream()
+                .filter(arg -> {
+                    try {
+                        return arg.getFlag().equals(flag);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    return false;
+                })
+                .findFirst()
+                .get()
+                .parseValue();
+    }
 }
